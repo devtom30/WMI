5800ca4c0ff5f48a96144976259214b0f48fc5d8
Thomas Lornet <tlornet@teclib.com>
2016-11-17 11:34:19 +0100

    further!

--- example/go.pl
+++ example/go.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/perl -W
 
 use strict;
+use lib '../lib';
 
 use DMTF::WSMan;
+use SOAP::Lite;
+use Data::Dumper;
+use XML::Parser;
+
 
 my $user = $ARGV[0];
 my $password = $ARGV[1];
@@ -34,7 +39,44 @@ $xml = $wsman->enumerate(
 	ResourceURI=>$uri
     },
     filter => '*'
-    );
-print $xml;
+);
+# print $xml;
 open O, ">" . 'response.xml';
-print O $xml
+print O $xml;
+
+my $xmls = extractXmlFromResponse($xml);
+$xml = $xmls->[0];
+
+open O2, ">" . 'debug.log';
+my $parser = XML::Parser->new(Style => 'Debug');
+print O2 'On parse NOW' . "\n";
+my $ret = $parser->parse($xml);
+if ($ret) {
+    print O2 "Yes, ça parse, c'est good" . "\n";
+} else {
+    print O2 "C'est PAS good, voici le retour :" . "\n";
+    print O2 $ret;
+}
+
+my $deserial = SOAP::Deserializer->new;
+my $obj = $deserial->deserialize($xml);
+my $dd = Data::Dumper->new([$obj]);
+print $dd->Dump;
+
+sub extractXmlFromResponse {
+    my ($response) = @_;
+
+    my $xmls = [];
+    my @split = split /</, $response;
+    my $xml = '';
+    my $line;
+    while ($line = shift @split) {
+	$xml .= $line;
+	if ($line =~ /\/s:Envelope>/) {
+	    push @$xmls, $xml;
+	    $xml = '';
+	}
+    }
+
+    return $xmls;
+}
