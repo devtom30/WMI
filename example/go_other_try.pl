a5df8a1e4502e36d15c7bc93f4df373547c30278
tlornet <tlornet@teclib.com>
2016-11-28 15:14:26 +0100

    try

--- example/go_other_try.pl
+++ example/go_other_try.pl
@@ -133,7 +133,7 @@ unless ($iRC == 0) {
 $arr = Variant( VT_ARRAY | VT_VARIANT | VT_BYREF  , [1,1] );
 $sPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
 # Do not use Die for this method
-my $iRC = $objReg->GetStringValue($Win32::Registry::HKEY_LOCAL_MACHINE,
+$iRC = $objReg->GetStringValue($Win32::Registry::HKEY_LOCAL_MACHINE,
     $sPath, $arr); # or die "Cannot fetch registry key :",
 print 'iRC : ' . $iRC . "\n";
 
