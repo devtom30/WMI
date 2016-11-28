f3bf4300914d4a31eb8b35e93c224c0050181070
tlornet <tlornet@teclib.com>
2016-11-28 10:55:37 +0100

    try

--- example/go_other_try.pl
+++ example/go_other_try.pl
@@ -94,8 +94,8 @@ foreach my $item ( in( $arr->Value ) ) {
 } # end foreach
 
 $arr = Variant( VT_ARRAY | VT_VARIANT | VT_BYREF  , [1,1] );
-#$sPath = "HARDWARE\\Description\\System\\BIOS";
-$sPath = "HARDWARE\\DESCRIPTION\\System";
+$sPath = "HARDWARE\\Description\\System\\BIOS";
+#$sPath = "HARDWARE\\DESCRIPTION\\System";
 # Do not use Die for this method
 $iRC = $objReg->EnumKey($HKEY_LOCAL_MACHINE,
     $sPath, $arr); # or die "Cannot fetch registry key :",
