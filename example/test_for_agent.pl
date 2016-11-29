470e2c25e4b80e5d0a7e875892faf9259c0d155d
tlornet <tlornet@teclib.com>
2016-11-29 15:30:37 +0100

    test win32 functions

--- example/test_for_agent.pl
+++ example/test_for_agent.pl
@@ -7,18 +7,13 @@ use Win32::OLE::Variant;
 
 use TheWin32;
 
-
 my $computer = $ARGV[2];
-my $user = $ARGV[0];
-my $pass = $ARGV[1];
+my $user     = $ARGV[0];
+my $pass     = $ARGV[1];
 
-my $service = TheWin32::_connectToService(
-    $computer,
-    $user,
-    $pass,
-    "root\\default"
-);
-if (!$service) {
+my $service =
+  TheWin32::_connectToService( $computer, $user, $pass, "root\\default" );
+if ( !$service ) {
     print 'sa race de service';
     exit;
 }
@@ -27,7 +22,7 @@ print 'service ok ';
 print "\n";
 
 my $objReg = $service->Get("StdRegProv");
-if (!$objReg) {
+if ( !$objReg ) {
     print 'sa race de objReg';
     exit;
 }
@@ -37,73 +32,83 @@ my $path = "HARDWARE/Description/System/BIOS/BIOSReleaseDate";
 $path =~ s/\//\\\\/g;
 print '$path is : ' . $path . "\n";
 print 'trying all paths ' . "\n";
-tryAllPath($objReg, $path);
+tryAllPath( $objReg, $path );
 
 my $p = "HARDWARE";
 my $e = "Description";
 print $p . ' - ' . $e . ' ? ';
-if ($objReg->EnumKey($Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e)) {
-    print 'ok'."\n";
+if ( $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e ) ) {
+    print 'ok' . "\n";
+}
+else {
+    print 'pas glop' . "\n";
 }
 
 $p = "HARDWARE\\Description\\System\\BIOS";
 $e = "BIOSReleaseDate";
 print $p . ' - ' . $e . ' ? ';
-if ($objReg->EnumKey($Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e)) {
-    print 'ok'."\n";
+if ( $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e ) ) {
+    print 'ok' . "\n";
 }
-
+else {
+    print 'pas glop' . "\n";
+}
 
 my $strKeyPath = "HARDWARE/Description/System/BIOS";
 $strKeyPath =~ s/\//\\\\/g;
 print '$strKeyPath is : ' . $strKeyPath . "\n";
 my $strEntryName = "BIOSReleaseDate";
-my $result = Variant(VT_BYREF()|VT_BSTR(),0);
-my $strValue = $objReg->GetStringValue($Win32::Registry::HKEY_LOCAL_MACHINE, $strKeyPath, $strEntryName);
-if (!$strValue || $strValue != 0) {
+my $result = Variant( VT_BYREF() | VT_BSTR(), 0 );
+my $strValue = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE,
+    $strKeyPath, $strEntryName );
+if ( !$strValue || $strValue != 0 ) {
     print 'usciamo di qua subito' . "\n";
     exit;
 }
 print 'strValue : ' . $strValue;
 
-$strValue = $objReg->GetStringValue($Win32::Registry::HKEY_LOCAL_MACHINE, $strKeyPath, $strEntryName, $result);
+$strValue = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE,
+    $strKeyPath, $strEntryName, $result );
 
 print 'result : ' . $result;
 
 sub tryAllPath {
-    my ($objReg, $path) = @_;
+    my ( $objReg, $path ) = @_;
 
     my @path = split /\\\\/, $path;
     my $pathToEntry = shift @path;
     my $entry;
 
     while (@path) {
-        if ($entry && $entry =~ /[a-z]+/) {
+        if ( $entry && $entry =~ /[a-z]+/ ) {
             $entry = uc $entry;
-        } else {
+        }
+        else {
             $entry = shift @path;
         }
-        if (tryPath($objReg, $pathToEntry, $entry)) {
+        if ( tryPath( $objReg, $pathToEntry, $entry ) ) {
             print 'good for ' . $pathToEntry . ' ' . $entry . "\n";
             $pathToEntry .= "\\" . $entry;
-        } else {
+        }
+        else {
             print 'foirade for ' . $pathToEntry . ' ' . $entry . "\n";
-            next if ($entry && $entry =~ /[a-z]+/);
+            next if ( $entry && $entry =~ /[a-z]+/ );
             last;
         }
     }
 }
 
 sub tryPath {
-    my ($objReg, $path, $entry) = @_;
+    my ( $objReg, $path, $entry ) = @_;
 
-
-    my $strValue = $objReg->EnumKey($Win32::Registry::HKEY_LOCAL_MACHINE, $path, $entry);
+    my $strValue =
+      $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $path, $entry );
 
     my $ret;
-    if (!$strValue || $strValue != 0) {
+    if ( !$strValue || $strValue != 0 ) {
         $ret = 0;
-    } else {
+    }
+    else {
         $ret = 1;
     }
     return $ret;
