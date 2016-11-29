a7b2d059ab1e1e638555555adb14c1780c6b764d
tlornet <tlornet@teclib.com>
2016-11-29 15:42:17 +0100

    test win32 functions

--- example/test_for_agent.pl
+++ example/test_for_agent.pl
@@ -37,7 +37,7 @@ tryAllPath( $objReg, $path );
 my $p = "HARDWARE";
 my $e = "Description";
 print $p . ' - ' . $e . ' ? ';
-my $res = $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
+my $res = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
 if ( $res && $res == 0 ) {
     print 'ok' . "\n";
 }
@@ -48,7 +48,7 @@ else {
 $p = "HARDWARE\\DESCRIPTION\\System\\BIOS";
 $e = "BIOSReleaseDate";
 print $p . ' - ' . $e . ' ? ';
-$res = $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
+$res = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
 if ( $res && $res == 0 ) {
     print 'ok' . "\n";
 }
@@ -59,7 +59,7 @@ else {
 $p = "HARDWARE\\DESCRIPTION\\System\\BIOS";
 $e = "BIOSReleaseDate";
 print $p . ' - ' . $e . ' ? ';
-$res = $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
+$res = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
 if ( $res && $res == 0 ) {
         print 'ok' . "\n";
 }
@@ -70,7 +70,7 @@ else {
 $p = "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0\\";
 $e = "ProcessorNameString";
 print $p . ' - ' . $e . ' ? ';
-$res = $objReg->EnumKey( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
+$res = $objReg->GetStringValue( $Win32::Registry::HKEY_LOCAL_MACHINE, $p, $e );
 if ( $res && $res == 0 ) {
     print 'ok' . "\n";
 }
