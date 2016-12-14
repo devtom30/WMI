1762a89e9ef78a59a0640a76ff5bf3e8b885cc36
tlornet <tlornet@teclib.com>
2016-12-14 17:56:11 +0100

    testing

--- example/test_for_agent.pl
+++ example/test_for_agent.pl
@@ -38,7 +38,7 @@ my $func = sub {
 #};
 #&$func if $@;
 eval {
-    my $rrr = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF()|Win32::OLE::Variant::VT_BSTR(),0);
+    my $rrr = Win32::OLE::Variant->new( Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_BYREF()  , [1,1] );
     my $retretret = $objReg->EnumKey($Win32::Registry::HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", $rrr);
     print 'proc name';
 };
