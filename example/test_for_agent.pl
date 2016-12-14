9a4a92e90b93179b43038d6044870b44de56cfe2
tlornet <tlornet@teclib.com>
2016-12-14 18:10:45 +0100

    testing

--- example/test_for_agent.pl
+++ example/test_for_agent.pl
@@ -51,6 +51,12 @@ eval {
 &$func if $@;
 eval {
     $rrr = Win32::OLE::Variant->new( Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_BYREF()  , [1,1] );    my $rrr = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF()|Win32::OLE::Variant::VT_BSTR(),0);
+    my $retretret = $objReg->EnumKey($Win32::Registry::HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0\\Quoi", $rrr);
+    print 'EnumKey Quoi (unexisting keyName) : ' . $retretret . "\n";
+};
+&$func if $@;
+eval {
+    $rrr = Win32::OLE::Variant->new( Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_BYREF()  , [1,1] );    my $rrr = Win32::OLE::Variant->new(Win32::OLE::Variant::VT_BYREF()|Win32::OLE::Variant::VT_BSTR(),0);
     my $retretret = $objReg->GetStringValue($Win32::Registry::HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
         "Quoi", $rrr);
     print 'Alors Quoi (unexisting keyName) : ' . $retretret . "\n";
