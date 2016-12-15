9c7c67ed99408365d97b52d1acb5a68c83bf76a7
tlornet <tlornet@teclib.com>
2016-12-15 18:12:29 +0100

    testing

--- example/test_for_agent2.pl
+++ example/test_for_agent2.pl
@@ -103,7 +103,7 @@ eval {
     my $arr1 = Win32::OLE::Variant->new( Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_BYREF()  , [1,1] );
     my $arr2 = Win32::OLE::Variant->new( Win32::OLE::Variant::VT_ARRAY() | Win32::OLE::Variant::VT_VARIANT() | Win32::OLE::Variant::VT_BYREF()  , [1,1] );
     $return = $objReg->EnumValues($hkey, $keyName, $arr1, $arr2);
-    my $err = Win32::OLE->LastError;
+    my $err = sprintf(Win32::OLE->LastError);
     print '$err ' . $err . "\n";
 
     if (2==1 && defined $return && $return == 0) {
