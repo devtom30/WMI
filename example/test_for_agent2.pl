984be5f655a29ae89c7e984b67295a9501ace7a3
tlornet <tlornet@teclib.com>
2016-12-15 16:09:51 +0100

    testing

--- example/test_for_agent2.pl
+++ example/test_for_agent2.pl
@@ -147,7 +147,8 @@ print "\n";
 
 my $arr = Variant( VT_ARRAY | VT_VARIANT | VT_BYREF  , [1,1] );
 
-my $sPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+#my $sPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+my $sPath = "SYSTEM\\CurrentControlSet\\Control\\Network";
 
 # Do not use Die for this method
 my $iRC = $objReg->EnumKey($HKLM,
